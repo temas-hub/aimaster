4a0de6254e1ca03369f08620c5edefea8c6f7319
Artem Zhdanov <artem.zhdanov@macys.com>
2016-01-25 19:19:34 +0200

    gradle 2.7 version migrated. 2.8+ hangs. Android gradle plugin updated to 1.5.0. Build script structure was simplified

--- core/src/main/kotlin/com/temas/aimaster/Multiplayer.kt
+++ core/src/main/kotlin/com/temas/aimaster/Multiplayer.kt
@@ -1,22 +1,54 @@
-package main.kotlin.com.temas.aimaster
+package com.temas.aimaster
 
 import io.netty.bootstrap.Bootstrap
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInitializer
 import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.DatagramPacket
 import io.netty.channel.socket.nio.NioDatagramChannel
+import io.netty.handler.codec.MessageToMessageDecoder
+import io.netty.handler.codec.protobuf.ProtobufEncoder
+import io.netty.util.concurrent.GenericFutureListener
 
 /**
  * @author Artem Zhdanov <azhdanov@griddynamics.com>
  * @since 21.01.2016
  */
 object Multiplayer {
+    val HOST = System.getProperty("host", "127.0.0.1");
+    val PORT = Integer.parseInt(System.getProperty("port", "8080"));
+
     val group = NioEventLoopGroup(1)
     val b = Bootstrap()
+    val packetToByteBufDecoder = object : MessageToMessageDecoder<DatagramPacket>() {
+        override fun decode(ctx: ChannelHandlerContext, msg: DatagramPacket, out: MutableList<Any>) {
+            out.add(msg.content().retain())
+        }
+    }
+    val handler = object : ChannelInitializer<NioDatagramChannel>() {
+        override fun initChannel(ch: NioDatagramChannel) {
+            val pipeline  = ch.pipeline()
+            pipeline.addLast(packetToByteBufDecoder, ProtobufEncoder())
+        }
+    }
 
     init{
-        b.group(group).channel(NioDatagramChannel::class.java)
+        b.group(group).channel(NioDatagramChannel::class.java).handler(handler)
     }
 
     fun register() {
-
+        // Start the connection attempt.
+        try {
+            val channelFuture = b.connect(HOST, PORT)
+            channelFuture.addListener(GenericFutureListener
+            { future ->
+                if (!future.isSuccess) {
+                    println("Error connecting to $HOST $PORT")
+                }
+             })
+            channelFuture.sync().channel()
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
